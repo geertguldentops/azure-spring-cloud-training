731507061b5842f80f08f327777a9164dd4ad533
Geert Guldentops <geert.guldentops@corsearch.com>
2020-06-11 10:50:18 +0200

    Add custom commands for step 2 + add cleanPackage.sh

---
+++ 02-build-a-simple-spring-boot-microservice/simple-microservice/cleanPackage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function setMavenOpts() {
+	export MAVEN_OPTS='-Xms4096m -Xmx4096m'
+}
+
+function setJavaHome() {
+	export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+}
+
+function doCleanPackage() {
+	mvn clean package
+}
+
+setMavenOpts
+setJavaHome
+
+doCleanPackage
